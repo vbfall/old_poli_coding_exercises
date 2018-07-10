907ec9d62a722d593b70f3271cdd2f60b2d1de6f
Vitor Falleiros <vbfall@gmail.com>
2018-07-10 00:33:02 -0400

    add date scan and length verification - vf

--- new_solutions/01-datanasc_2018.c
+++ new_solutions/01-datanasc_2018.c
@@ -10,7 +10,61 @@
   b. how many days have passed from that past date until today
   Use time.h is forbidden.*/
 
+#include<stdbool.h>
 #include<stdio.h>
+#include<string.h>
+
+char* get_date(char date_type[]);
+
+bool verify_date(char date[]);
 
 void main()
-{}
+{
+    char* current_date = "0000-00-00";
+    char* target_date = "0000-00-00";
+
+    printf("\n");
+    current_date = get_date("current");
+    target_date = get_date("target");
+
+//dcheck = calculate difference in days between given date and reference
+//dcurrent = calculate difference in days between today and reference
+
+//calculate difference in days between check date and current date using dcheck and dcurrent
+
+//calculate weekday for each date using dcheck and dcurrent
+
+    printf("\nToday, %s, is a ____. Target date given was %s which was a _____, ___ days ago.\n",
+        current_date, target_date);
+}
+
+char* get_date(char date_type[])
+{
+    bool missing_date = true;
+    static char full_date[] = "0000-00-00";
+    while(missing_date)
+    {
+        printf("Please give %s date (yyyy-mm-dd): ",date_type);
+        scanf("%s",full_date);
+        if(verify_date(full_date))
+        {
+            missing_date = false;
+        }
+        else
+        {
+            printf("\nInvalid date, please mind the input format and try again.\n\n");
+        }
+    }
+    return full_date;
+} /*end of get_date */
+
+bool verify_date(char date[])
+{
+    bool check = false;
+    int l = strlen(date);
+    if(l == 10 /*and dashes in the right positions*/ /*and all others are digits*/)
+    {
+        check = true;
+    }
+    return check;
+} /*end of verify_date */
