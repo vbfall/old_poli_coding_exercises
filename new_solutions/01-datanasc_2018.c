0986609efcb4646c14ce494bc556528312c8db57
Vitor Falleiros <vbfall@gmail.com>
2018-07-09 22:05:33 -0400

    add start of new solutions-vf

---
+++ new_solutions/01-datanasc_2018.c
@@ -0,0 +1,16 @@
+/***************************************************************************
+ ***                                                                     ***
+ ***   Exercise #1 from MAC-0115 discipline, 1998                        ***
+ ***   Originally proposed by professor Dr. Routo Terada                 ***
+ ***                                                                     ***
+ ***************************************************************************/
+
+/*Goal is to receive current date and another date in the past, and to return:
+  a. what day of the week was the past date given
+  b. how many days have passed from that past date until today
+  Use time.h is forbidden.*/
+
+#include<stdio.h>
+
+void main()
+{}
