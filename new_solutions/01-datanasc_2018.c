773c4b54ace4ba4c878022f5b906e0f3d64cb2d1
Vitor Falleiros <vbfall@gmail.com>
2018-07-12 13:38:48 -0400

    fix date input and validation

--- new_solutions/01-datanasc_2018.c
+++ new_solutions/01-datanasc_2018.c
@@ -14,57 +14,115 @@
 #include<stdio.h>
 #include<string.h>
 
-char* get_date(char date_type[]);
+// check that the dashes in a date string are in the correct position
+// and only in the correct position
+bool check_dashes(char date[]);
 
+// Gets date_type[] date from user, as a string, and stores it into date[]
+void get_date(char date_type[], char date[]);
+
+// checks if date is valid
+bool valid_date(char date[]);
+
+// Verifies if a string is in the correct date format yyyy-mm-dd
 bool verify_date(char date[]);
 
 int main(void)
 {
-    char* current_date = "0000-00-00";
-    char* target_date = "0000-00-00";
+    char current_date[] = "0000-00-00";
+    char target_date[] = "0000-00-00";
 
-    // both are storing the same value (the one supplied later).
-    // Something to do with the definition inside the function, but how to get around it?
     printf("\n");
-    current_date = get_date("current");
-    target_date = get_date("target");
+    get_date("current",current_date);
+    get_date("target",target_date);
 
-//dcheck = calculate difference in days between given date and reference
+//dtarget = calculate difference in days between given date and reference
 //dcurrent = calculate difference in days between today and reference
 
-//calculate difference in days between check date and current date using dcheck and dcurrent
+//calculate difference in days between check date and current date using dtarget and dcurrent
 
-//calculate weekday for each date using dcheck and dcurrent
+//calculate weekday for each date using dtarget and dcurrent
 
-    printf("\nToday, %s, is a ____. Target date given was %s which was a _____, ___ days ago.\n",
+    printf("\nToday, %s, is a ____. Target date given was %s "
+        "which was a _____, ___ days ago.\n",
         current_date, target_date);
+    return 0;
 }
 
-char* get_date(char date_type[])
+bool check_dashes(char date[])
+{
+    int i = 0, l = strlen(date);
+    bool check = true;
+    while(check && i < l)
+    {
+        switch(i)
+        {
+            case 4:
+            case 7:
+                if(date[i] != '-'){check = false;}
+                break;
+            default:
+                if(date[i] < '0' || date[i] > '9'){check = false;}
+        }
+        i++;
+    }
+    return check;
+} // end of check_dashes
+
+void get_date(char date_type[], char date[])
 {
     bool missing_date = true;
-    static char full_date[] = "0000-00-00";
     while(missing_date)
     {
-        printf("Please give %s date (yyyy-mm-dd): ",date_type);
-        scanf("%s",full_date);
-        if(verify_date(full_date))
+        printf("Please give the %s date (yyyy-mm-dd): ",date_type);
+        scanf("%s",date);
+        if(verify_date(date))
         {
             missing_date = false;
         }
         else
         {
-            printf("\nInvalid date, please mind the input format and try again.\n\n");
+            printf("\nInvalid date, please mind the input format "
+                "and try again.\n\n");
         }
     }
-    return full_date;
 } /*end of get_date */
+
+bool valid_date(char date[])
+{
+    bool check = false;
+    int year = ((int) (date[0] - '0'))*1000 + ((int) (date[1] - '0'))*100 + ((int) (date[2] - '0'))*10 + (int) (date[3] - '0');
+    int month = ((int) (date[5] - '0'))*10 + (int) (date[6] - '0');
+    int day = ((int) (date[8] - '0'))*10 + (int) (date[9] - '0');
+    if(month >= 1 && month <= 12)
+    {
+        if(day > 0)
+        {
+            switch(month)
+            {
+                case 2:
+                    if(day <= 28 || (day == 29 && year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)))
+                    {check = true;}
+                    break;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    if(day <= 30){check = true;}
+                    break;
+                default:
+                    if(day <= 31){check = true;}
+            }
+        }
+    }
+    return check;
+} // end of valid_date
 
 bool verify_date(char date[])
 {
     bool check = false;
-    int l = strlen(date);
-    if(l == 10 /*and dashes in the right positions*/ /*and all others are digits*/)
+    if(strlen(date) == 10 && check_dashes(date)
+        && valid_date(date))
     {
         check = true;
     }
